9a025b42c5220e1493d28ec6a23b639ec9e357b4
OXDBXKXO <louischevalier21@gmail.com>
2018-10-21 18:48:46 +0200

    Bloc, Line and Character detection

--- Segmentation.h
+++ Segmentation.h
@@ -1,14 +1,22 @@
 #ifndef SEGMENTATION_H
 #define  SEGMENTATION_H
 
-void CheckNeighbours(int x, int y, double **matrix);
-double** ConstructMatrix(size_t line, size_t column);
+typedef struct {
+ unsigned char *array;
+ size_t used;
+ size_t size;
+} Array ;
+
+unsigned char *Bloc_Detection(unsigned char **matrix);
+unsigned char *Labelling_GetCharacters();
+unsigned char *Labelling(unsigned char **matrix, int width, int height);
+unsigned char *Line_Detection(unsigned char **matrix, unsigned char *xblocs);
+void Bloc_Line_Detection(unsigned char **matrix);
+void CheckNeighbours(int x, int y);
 void freeArray(Array *a);
 void initArray(Array *a, size_t initialSize);
-void insertArray(Array *a, double element);
-void Labelling_FirstPass(double **matrix);
-Array Labelling_GetCharacters();
+void insertArray(Array *a, unsigned char element);
+void Labelling_FirstPass(unsigned char **matrix);
 void Labelling_SecondPass();
-Array Labelling(double **matrix);
 
 #endif
