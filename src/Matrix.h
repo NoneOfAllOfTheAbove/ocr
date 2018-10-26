491787b3176fc15801e2b0e6fac3864af84fcbe7
Theo Lepage <theo.lepage@outlook.com>
2018-10-26 22:48:27 +0200

    Started working on characters detection. Now handle IMG_Load errors. Lines are better organized.

--- src/Matrix.h
+++ src/Matrix.h
@@ -7,4 +7,6 @@ double **CreateDoubleMatrix(size_t line, size_t column);
 
 void FreeMatrix(double**, size_t);
 
+unsigned char **GetSubMatrix(unsigned char **matrix, int x1, int y1, int x2, int y2);
+
 #endif
