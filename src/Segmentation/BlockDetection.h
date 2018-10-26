37717d11ac2ecffd247a3f3d4a864aa75fbe0de1
Théo <theo.lepage@outlook.com>
2018-10-26 03:28:42 +0200

    Implemented lines detection. At the moment, I am working on a way to organize/store/link blocks, lines and characters.

--- src/Segmentation/BlockDetection.h
+++ src/Segmentation/BlockDetection.h
@@ -4,5 +4,6 @@
 unsigned char** DetectBlocks(unsigned char**, int, int);
 void Labeling(unsigned char **matrix, int x, int y, int width, int height, int*, int*, int);
 int **GetBlocks(unsigned char** matrix, int imageWidth, int imageHeight);
+int **GetLines(unsigned char **matrix, int **blocks, int imageWidth, int imageHeight);
 
 #endif
