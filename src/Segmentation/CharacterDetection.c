2d1e3d9e3f400a88da9b39a3d8e629253bf0dba4
Theo Lepage <theo.lepage@outlook.com>
2018-10-27 22:36:08 +0200

    Implemented characters detection.

--- src/Segmentation/CharacterDetection.c
+++ src/Segmentation/CharacterDetection.c
@@ -2,6 +2,39 @@
 
 #include "../Matrix.h"
 
+void __DetectCharacters(unsigned char **matrix, int x, int y, int *cx1, int *cy1, int *cx2, int *cy2)
+{
+	if(matrix[y][x] == 0)
+	{
+		return;
+	}
+
+	matrix[y][x] = 0;
+	
+	if(x < *cx1)
+		*cx1 = x;
+
+	if(x > *cx2)
+		*cx2 = x;
+
+	if(y < *cy1)
+		*cy1 = y;
+
+	if(y > *cy2)
+		*cy2 = y;
+	
+	__DetectCharacters(matrix, x + 1, y, cx1, cy1, cx2, cy2);
+	__DetectCharacters(matrix, x - 1, y, cx1, cy1, cx2, cy2);
+	__DetectCharacters(matrix, x, y + 1, cx1, cy1, cx2, cy2);
+	__DetectCharacters(matrix, x, y - 1, cx1, cy1, cx2, cy2);
+	
+	__DetectCharacters(matrix, x + 1, y + 1, cx1, cy1, cx2, cy2);
+	__DetectCharacters(matrix, x - 1, y + 1, cx1, cy1, cx2, cy2);
+	__DetectCharacters(matrix, x + 1, y - 1, cx1, cy1, cx2, cy2);
+	__DetectCharacters(matrix, x - 1, y - 1, cx1, cy1, cx2, cy2);
+
+}
+
 void DetectCharacters(unsigned char **binarizedImageMatrix, int imageWidth, int imageHeight, int **blocks, int blockNumber, int **lines)
 {
 	unsigned char **matrix = CreateCharMatrix(imageWidth, imageHeight);
@@ -26,14 +59,37 @@ void DetectCharacters(unsigned char **binarizedImageMatrix, int imageWidth, int 
 			lastY = y2;
 
 			// Look for characters
-			for(int y = y1; y < y2; y++)
+			for(int x = x1; x < x2; x++)
 			{
-				for(int x = x1; x < x2; x++)
+				for(int y = y1; y < y2; y++)
 				{
-					// find cx1 cy1 cx2 cy2
-					// unsigned char **characterMatrix = GetSubMatrix(matrix, cx1, cy1, cx2, cy2);
-					// put in matrix of 16x16 RESIZE
-					// OnCharacterDetected(characterMatrix);
+					if(matrix[y][x] == 1)
+					{
+						int cx1 = x, cy1 = y, cx2 = x, cy2 = y;
+						__DetectCharacters(matrix, x, y, &cx1, &cy1, &cx2, &cy2);
+						//printf("%d %d %d %d\n", cx1, cy1, cx2, cy2);
+						if(cx2 - cx1 != 0 && cy2 - cy1 != 0)
+						{
+							cx2 += 1;
+							cy2 += 1;
+							unsigned char **characterMatrix = GetSubMatrix(binarizedImageMatrix, cx1, cy1, cx2, cy2);
+							printf("\n");	
+							for(int b = 0; b < cy2 - cy1; b++)
+							{
+								for(int a = 0; a < cx2 - cx1; a++)
+								{
+									if(characterMatrix[b][a] == 1)
+									{
+										printf("%d ", characterMatrix[b][a]);
+									} else {
+										printf("  ");
+									}
+								}
+								printf("\n");
+							}
+						}
+						// put in matrix of 16x16 (RESIZE)
+					}
 				}
 			}
 		}
