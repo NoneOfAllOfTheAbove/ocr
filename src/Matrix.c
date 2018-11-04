e3d83db7f6f48b0544679d82b01a50471c2c6918
Theo Lepage <theo.lepage@outlook.com>
2018-11-04 15:27:29 +0100

    Implemented simple matrix resizing to make segmentation ouputs match NN inputs.

--- src/Matrix.c
+++ src/Matrix.c
@@ -58,3 +58,74 @@ unsigned char **GetSubMatrix(unsigned char **matrix, int x1, int y1, int x2, int
 
 	return result;
 }
+
+unsigned char **ResizeMatrix(unsigned char **matrix, int oldX, int oldY)
+{
+	// Step 1: Put matrix in a new matrix of size a * (16 * 16) 	
+	int side = oldX;
+	if(oldY > oldX)
+	{
+		side = oldY;
+	}
+	if(side % 16 != 0)
+	{
+		side = (1 + side / 16) * 16;
+	} else
+	{
+		side = (side / 16) * 16;	
+	}
+
+	unsigned char **result = CreateCharMatrix(side, side);
+	
+	for(int y = 0; y < side; y++)
+	{
+		for(int x = 0; x < side; x++)
+		{
+			result[y][x] = 0;
+		}
+	}
+	int leftOffset = (side - oldX) / 2;
+	int topOffset = (side - oldY) / 2;
+	for(int y = 0; y < oldY; y++)
+	{
+		for(int x = 0; x < oldX; x++)
+		{
+			result[y + topOffset][x + leftOffset] = matrix[y][x];
+		}
+	}
+
+	// Step 2: Scale down matrix with side > 16
+	int scale = side / 16;
+	if(scale > 1)
+	{
+		unsigned char **resized = CreateCharMatrix(16, 16);
+		
+		for(int y = 0; y < 16; y++)
+		{
+			for(int x = 0; x < 16; x++)
+			{
+				// 1 pixel in resized is scale pixel in result
+				unsigned char sum = 0;
+				for(int y1 = 0; y1 < scale; y1++)
+				{
+					for(int x1 = 0; x1 < scale; x1++)
+					{
+						sum += result[scale * y + y1][scale * x + x1];
+					}
+				}
+				if(sum > 0)
+				{
+					resized[y][x] = 1;
+				}
+				else
+				{
+					resized[y][x] = 0;
+				}
+			}
+		}
+
+		result = resized;
+	}
+
+	return result;
+}
