6aef1bb0c39666c8157c4414739929384f3bd677
Théo <theo.lepage@outlook.com>
2018-11-14 22:26:31 +0100

    Fixed a bug with stacks loosing information, now they use Coord objects instead of int[2]. All algorithms are now iterative ! Crash.png doesn't crash anymore ;).

--- src/Stack.h
+++ src/Stack.h
@@ -1,19 +1,26 @@
 #ifndef STACK_H
 #define STACK_H
 
+typedef struct Coord Coord;
+struct Coord
+{
+	int x;
+	int y;
+};
+
 typedef struct Stack Stack;
 struct Stack
 {
 	int size;
 	int top;
-	int** stack;
+	Coord* stack;
 };
 
-Stack StackCreate(int size);
+void StackCreate(Stack *stack, int size);
 int IsStackFull(Stack *stack);
 int IsStackEmpty(Stack *stack);
-int *PeekStack(Stack *stack);
-int *StackPop(Stack *stack);
-void StackPush(Stack *stack, int *data);
+Coord StackPeek(Stack *stack);
+Coord StackPop(Stack *stack);
+void StackPush(Stack *stack, Coord element);
 
 #endif
